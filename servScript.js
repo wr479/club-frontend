5e937c1a32998e66c496a23deced21d53b9baf9c
egorb2302 <zxcgovnoedig@gmail.com>
2026-01-13 00:50:49 +0400

    гитхаб блядина

---
+++ servScript.js
@@ -0,0 +1,55 @@
+let activeId = null;
+
+const labels = document.querySelectorAll('.wrap-label');
+const triagles = document.querySelectorAll('.triagle-with-pic');
+
+const pic1 = document.querySelector('#triagle1');
+const pic2 = document.querySelector('#triagle2');
+const pic3 = document.querySelector('#triagle3');
+
+const border1 = document.querySelector('#border1');
+const border2 = document.querySelector('#border2');
+const border3 = document.querySelector('#border3');
+
+
+labels.forEach((label) => {
+    label.addEventListener('click', () => {
+        if (activeId === label.id) return;
+
+        labels.forEach(i => i.classList.remove('active'));
+
+        label.classList.add('active');
+
+        activeId = label.id;
+        triaglesFunc();
+    });
+}); // с этой частью кода помогла нейронка
+
+function triaglesFunc() {
+        if (activeId === 'label1') {
+            pic1.style.display = 'block';
+            border1.style.backgroundImage = 'url(./services/active-label.png)'; 
+            pic2.style.display = 'none';
+            border2.style.backgroundImage = 'url(./services/passive-label.png)';
+            pic3.style.display = 'none';
+            border3.style.backgroundImage = 'url(./services/passive-label.png)';
+        } else if (activeId === 'label2') {
+            pic1.style.display = 'none';
+            border1.style.backgroundImage = 'url(./services/passive-label.png)';
+            pic2.style.display = 'block';
+            border2.style.backgroundImage = 'url(./services/active-label.png)';
+            pic3.style.display = 'none';
+            border3.style.backgroundImage = 'url(./services/passive-label.png)';
+        } else if (activeId === 'label3') {
+            pic1.style.display = 'none';
+            border1.style.backgroundImage = 'url(./services/passive-label.png)';
+            pic2.style.display = 'none';
+            border2.style.backgroundImage = 'url(./services/passive-label.png)';
+            pic3.style.display = 'block';
+            border3.style.backgroundImage = 'url(./services/active-label.png)';
+        };
+}; // а тут логику сам писал ( видно потому, что не аккуратно )
+
+triaglesFunc();
+
+console.log(triagles)
