c14ae71fcb48a091c2883163360b7b41cb58c071
egorb2302 <zxcgovnoedig@gmail.com>
2026-02-01 20:53:21 +0400

    add new catalog page ( need some fixes )

---
+++ jsScripts/pageChange.js
@@ -0,0 +1,9 @@
+const titles = document.querySelectorAll('.accum-info > h1');
+const pages = document.querySelectorAll('.sortpages-page');
+
+pages.forEach(function(e) {
+    if (e.className == 'sortpages-page for-order') {
+        
+    };
+}) 
+console.log(pages);
