021917dbd5b39d9948b2537f70c14cca2fcd9d62
egorb2302 <zxcgovnoedig@gmail.com>
2026-02-02 17:25:08 +0400

    closely fin to page ( fix bucket btn )

--- jsScripts/pageChange.js
+++ jsScripts/pageChange.js
@@ -1,9 +1,26 @@
-const titles = document.querySelectorAll('.accum-info > h1');
-const pages = document.querySelectorAll('.sortpages-page');
+const orderedPages = document.querySelectorAll('.sortpages-page.for-order > .accum-info > h1');
+orderedPages.forEach(function (e) {
+    e.style.color = 'rgba(29, 29, 29, 1)';
+});
 
-pages.forEach(function(e) {
-    if (e.className == 'sortpages-page for-order') {
-        
-    };
-}) 
-console.log(pages);
+const availiblePages = document.querySelectorAll('.sortpages-page.in-availebility > .accum-info > h1');
+availiblePages.forEach(function (e) {
+    e.style.color = 'rgba(29, 29, 29, 1)';
+})
+
+const hideBtn = document.querySelectorAll('.choosen-bucket-btn-cont');
+hideBtn.forEach(function (btn) {
+    btn.addEventListener('click', function() {
+        const cont = btn.closest('.sortpages-page');
+        if (cont) {
+            const hideTheContent = cont.querySelector('.choosen-addItem-btn-cont');
+            const btnAfterClick = cont.querySelector('.choosen-bucket-btn-cont');
+            const contAfterClick = cont.querySelector('.choosen-bucket-btn-cont-hidden');
+            hideTheContent.style.visibility = 'hidden';
+            btnAfterClick.style.display = 'none';
+            contAfterClick.style.display = 'flex';
+        }
+    }, false);
+});
+
+
