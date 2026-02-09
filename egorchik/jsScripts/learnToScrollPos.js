2b054f73b3c7100a5a79681b851bdc9d235419b9
egorb2302 <zxcgovnoedig@gmail.com>
2026-02-09 17:01:55 +0400

    ....

--- egorchik/jsScripts/learnToScrollPos.js
+++
@@ -1,6 +0,0 @@
-window.onbeforeunload = () => sessionStorage.setItem('scrollPos', window.scrollY);
-
-window.onload = () => {
-    const scrollPos = parseInt(sessionStorage.getItem('scrollPos')) || 0;
-    window.scrollTo(0, scrollPos);
-};
