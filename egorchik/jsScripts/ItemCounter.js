1c83bd81a286cce02e8864a22156e875335d02a3
egorb2302 <zxcgovnoedig@gmail.com>
2026-03-09 17:18:41 +0400

    add logic for one of old scripts on project

--- egorchik/jsScripts/ItemCounter.js
+++ egorchik/jsScripts/ItemCounter.js
@@ -1,26 +1,52 @@
 const plus = document.querySelector('#plusCount');
 const minus = document.querySelector('#minusCount');
 let valueOfButton = document.querySelector('#counter');
-let counter = -3;
+let counter = 0;
 let plug = '00';
 
 function plusMinus() {
-    if (counter < 0) {
-        valueOfButton.textContent = (counter = 0);
-    }
-
     plus.addEventListener('click', () => { 
     counter++;
-    valueOfButton.textContent = '0' + counter;
+    valueOfButton.textContent = counter;
     }, false);
 
-    minus.addEventListener('click', () => { 
-    counter--;
-    valueOfButton.textContent = '0' + counter;
+    minus.addEventListener('click', () => {
+        if (counter > 0) {
+            counter--;
+            valueOfButton.textContent = counter;
+        } 
     }, false);   
 }
 
 plusMinus();
+
+const addBtn = document.querySelector('.choosen-bucket-btn-cont');
+const priceOfItem = document.querySelector('.price-cont > h1');
+const titleOfItem = 'MAGNUM 60Ah';
+const emkostOfItem = document.querySelector('#emkost');
+const quantityOfItem = valueOfButton;
+addBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    let obj = {
+        name: titleOfItem,
+        emkost: Number(emkostOfItem.getAttribute('data-val')),
+        price: parseInt(priceOfItem.textContent) * Number(quantityOfItem.textContent),
+        quantity: Number(quantityOfItem.textContent),
+    }
+
+    fetch('http://localhost:3000/cart', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj),
+    }).then((response) => {
+        return response.json()
+    }).then((json) => {
+        console.log(json)
+    })
+})
 
 // plus.addEventListener('click', () => { 
 //     counter++;
