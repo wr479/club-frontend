ddfe893ba373f59bdc89dbc3f6151884ecd139bd
egorb2302 <zxcgovnoedig@gmail.com>
2026-03-17 15:54:50 +0400

    add some custom alerts for validation

--- egorchik/jsScripts/formValidation.js
+++ egorchik/jsScripts/formValidation.js
@@ -1,9 +1,57 @@
 const currentForm = document.forms[0];
+
+const nameInputEl = document.querySelector('[data-name-input]');
+const phoneInputEl = document.querySelector('[data-phone-input]');
+const mailInputEl = document.querySelector('[data-mail-input]');
+
+function removeErr(divEl, input) {
+    input.style.border = "4px solid black";
+    let errorEl = divEl.nextSibling
+    if (errorEl === null) {
+        return
+    } else {
+        errorEl.remove()
+    }
+}
+
+const nameInput = document.querySelector('.name-input');
+
+nameInput.addEventListener('blur', () => {
+    removeErr(nameInputEl, nameInput);
+
+    if (nameInput.value.length < 3 || nameInput.value === "") {
+        nameInput.style.border = "4px solid rgba(255, 0, 0, 1)";
+        nameInputEl.insertAdjacentHTML('afterend', '<p class="error-message">Заполните поле</p>')
+    }
+})
+
+const phoneInput = document.querySelector('.phone-input');
+
+phoneInput.addEventListener('blur', () => {
+    removeErr(phoneInputEl, phoneInput);
+
+    if (phoneInput.value.length < 11 || phoneInput.value === "") {
+        phoneInput.style.border = "4px solid rgba(255, 0, 0, 1)";
+        phoneInputEl.insertAdjacentHTML('afterend', '<p class="error-message">Заполните поле</p>')
+    }
+})
+
+const mailInput = document.querySelector('.mail-input');
+
+mailInput.addEventListener('blur', () => {
+
+    if (mailInput.value === '') {
+        return 
+    } else if (!mailInput.value.includes("@")) {
+        mailInput.style.border = "4px solid rgba(255, 0, 0, 1)";
+        mailInputEl.insertAdjacentHTML('beforeend', '<p class="error-message">Неверный формат</p>')
+    }
+})
+
 currentForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const formData = new FormData(currentForm)
-
     const userData = Object.fromEntries(formData)
 
     fetch('http://localhost:3000/users', {
