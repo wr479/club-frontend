2eda77ddb9008a3d7b421ac4d4ec37743deaa979
egorb2302 <berendeevegor@gmail.com>
2026-04-09 22:18:23 +0400

    fix one modal window

--- egorchik/jsScripts/popupValid.js
+++ egorchik/jsScripts/popupValid.js
@@ -1,8 +1,14 @@
 const openBtn = document.querySelector('.nav-button > button');
 
+function switchDisplay(el1, el2) {
+    el1.style.display = 'block';
+    el2.style.display = 'none'
+}
+
 openBtn.addEventListener('click', () => {
     const popup1 = document.querySelector('.modal-overlay');
-    const closePopup = document.querySelector('.krest');
+    const popupAccept = document.querySelector('.pop-up2');
+    const closePopup = document.querySelectorAll('.krest');
 
     if (!popup1.style.display === 'flex') {
         popup1.style.display = 'none'
@@ -10,11 +16,16 @@ openBtn.addEventListener('click', () => {
 
     popup1.style.display = 'flex';
 
-    closePopup.addEventListener('click', () => {
-        popup1.style.display = 'none';
+    closePopup.forEach((el) => {
+        el.addEventListener('click', () => {
+            popup1.style.display = 'none';
+    }   )
     })
     
     const popupForm = document.querySelector('.input-section');
+    const popup1Content = document.querySelector('.pop-up1');
+
+    switchDisplay(popup1Content, popupAccept)
 
     popupForm.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -22,6 +33,8 @@ openBtn.addEventListener('click', () => {
         const formdata = new FormData(popupForm);
         const userFormData = Object.fromEntries(formdata)
         console.log(userFormData)
+
+        switchDisplay(popupAccept, popup1Content)
     })
 })
 
